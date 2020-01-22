d712e41951353eaf6244c27f05f8bb4ae1ec857a
Erfan Al-Hossami <ealhossa@uncc.edu>
2020-01-21 23:59:51 -0500

    works

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/sigma-vue' : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/ealhossa" : "/",
+  chainWebpack: config => config.resolve.symlinks(false)
+};
